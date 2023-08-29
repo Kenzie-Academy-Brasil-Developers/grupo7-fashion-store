c7ea84fc22ce56cbe5e87ef01fd4046f56da8cf0
bervalim <bernardogvalim@gmail.com>
2023-08-29 19:52:29 -0300

    implementing-toasts

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./providers/UserContext.jsx";
-import { ToastContainer, toast } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,7 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <UserProvider>
         <App />
       </UserProvider>
-      <ToastContainer />
     </BrowserRouter>
   </React.StrictMode>
 );
