e47a9a496e0e8a8d433a0e9ec002d668f4234938
hugo <harodrigues257@gmail.com>
2023-08-29 15:38:41 -0300

    feat/login

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,5 +1,50 @@
+import { Link } from "react-router-dom";
+import { DefaultTemplate } from "../DefaultTemplate";
+import { Input } from "../Input/input";
+import { useForm } from "react-hook-form";
+import { registerSchema } from "../RegisterPage/formSchemaRegister";
+import { loginSchema } from "./formSchemaLogin";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserContext";
+import logo from "../../assets/login.png";
+
 export const LoginPage = () => {
-    return (
-        <h1>Login</h1>
-    )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const { submitLogin } = useContext(UserContext);
+
+  return (
+    <DefaultTemplate>
+      <section>
+        <img src={logo} alt="logo login" />
+        <form onSubmit={handleSubmit(submitLogin)}>
+          <h2>ENTRAR</h2>
+          <Input
+            type="text"
+            placeholder="E-MAIL"
+            {...register("email")}
+            error={errors.email}
+          />
+          <Input
+            type="password"
+            placeholder="SENHA"
+            {...register("password")}
+            error={errors.password}
+          />
+
+          <div>
+            <button>ACESSAR</button>
+            <Link to="/register">CADASTRE-SE</Link>
+          </div>
+        </form>
+      </section>
+    </DefaultTemplate>
+  );
+};
